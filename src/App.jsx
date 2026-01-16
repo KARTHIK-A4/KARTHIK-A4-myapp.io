7536b3198f31603495102f73ef91144204111a99
KARTHIK-A4 <karthikmm0099@gmail.com>
2026-01-16 12:21:19 +0530

    add:src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/navbar";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/dashboard";
