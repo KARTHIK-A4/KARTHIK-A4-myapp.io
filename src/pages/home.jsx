7cebd48f33e7748d663445575273b145b07310ce
KARTHIK-A4 <karthikmm0099@gmail.com>
2026-01-18 12:33:18 +0530

    add:src/assets/images/service-home.jpg

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,43 @@
 import React from "react";
+import img from "../assets/images/service-home.jpg"; 
 
 export default function Home() {
   return (
     <main style={styles.main}>
-      <h1>Welcome to Service Request System</h1>
-      <p>
-        This is the home page. Please register or login to continue.
-      </p>
+      {/* Fullscreen background image */}
+      <img src={img} alt="Service Home" style={styles.fullImage} />
+
+      {/* Overlay text */}
+      <div style={styles.overlay}>
+        <h1>Welcome to Service Request System</h1>
+        <p>This is the home page. Please register or login to continue.</p>
+      </div>
     </main>
   );
 }
 
 const styles = {
   main: {
-    padding: "40px",
+    position: "relative",
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+  },
+  fullImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",   
+    zIndex: 0,           
+  },
+  overlay: {
+    position: "relative",
+    zIndex: 1,           
+    color: "white",
     textAlign: "center",
+    paddingTop: "20%",
+    textShadow: "2px 2px 4px rgba(0,0,0,0.7)",
   },
 };
